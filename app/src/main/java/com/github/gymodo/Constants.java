f863eca9685c1eb029c4dd6e2c55926eae460872
Edgar <git@edgarluque.com>
2021-04-22 17:43:42 +0200

    constants

---
+++ app/src/main/java/com/github/gymodo/Constants.java
@@ -0,0 +1,14 @@
+package com.github.gymodo;
+
+public abstract class Constants {
+    public static final String COLLECTION_USERS = "users";
+
+    public static final String COLLECTION_FOODS = "foods";
+    public static final String COLLECTION_DIETS = "diets";
+    public static final String COLLECTION_MEALS = "meals";
+
+    public static final String COLLECTION_ROUTINES = "routines";
+    public static final String COLLECTION_SERIES = "series";
+    public static final String COLLECTION_EXERCISES = "exercises";
+    public static final String COLLECTION_MUSCLES = "muscles";
+}
