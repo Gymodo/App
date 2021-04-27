97ca7e67816ff1a520108edcccc969821c126bef
Edgar <git@edgarluque.com>
2021-04-27 17:47:00 +0200

    adapter folder

--- app/src/main/java/com/github/gymodo/MainActivity.java
+++ app/src/main/java/com/github/gymodo/MainActivity.java
@@ -8,22 +8,15 @@ import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.viewpager.widget.ViewPager;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.Toast;
 
-import com.google.android.gms.dynamic.IFragmentWrapper;
+import com.github.gymodo.adapters.ViewPagerAdapter;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 
