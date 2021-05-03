495e608c963e396b7f554e4f72ffe9b3972643e6
Edgar <git@edgarluque.com>
2021-05-03 17:31:53 +0200

    progress

--- app/src/main/java/com/github/gymodo/fragments/AddWorkoutFragment.java
+++ app/src/main/java/com/github/gymodo/fragments/AddWorkoutFragment.java
@@ -2,9 +2,12 @@ package com.github.gymodo.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.recyclerview.selection.ItemKeyProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,6 +19,7 @@ import com.github.gymodo.R;
 import com.github.gymodo.adapters.SeriesAdapter;
 import com.github.gymodo.exercise.Serie;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
@@ -34,6 +38,8 @@ public class AddWorkoutFragment extends Fragment {
     RecyclerView selected;
     List<Serie> seriesAvailable;
     List<Serie> seriesSelected;
+    TextInputEditText textName;
+    TextInputEditText textDesc;
 
     public AddWorkoutFragment() {
         // Required empty public constructor
@@ -61,6 +67,8 @@ public class AddWorkoutFragment extends Fragment {
 
         available = view.findViewById(R.id.AddWorkoutAvailableList);
         selected = view.findViewById(R.id.AddWorkoutSelectedList);
+        textName = view.findViewById(R.id.AddWorkoutNameInput);
+        textDesc = view.findViewById(R.id.AddWorkoutDescInput);
 
         adapterAvailable = new SeriesAdapter(seriesAvailable, view.getContext());
         adapterSelected = new SeriesAdapter(seriesSelected, view.getContext());
@@ -70,6 +78,8 @@ public class AddWorkoutFragment extends Fragment {
 
         selected.setLayoutManager(new LinearLayoutManager(view.getContext()));
         selected.setAdapter(adapterSelected);
+
+
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
         String userId = auth.getCurrentUser().getUid();
