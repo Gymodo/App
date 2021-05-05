c7c095f7e4fb45c2e4c7fd59cd5cfbca65cbbd4b
shahsawar <shahsawarbadshah@gmail.com>
2021-05-05 18:00:54 +0200

    progress

--- app/src/main/java/com/github/gymodo/adapters/UserReservationsAdapter.java
+++ app/src/main/java/com/github/gymodo/adapters/UserReservationsAdapter.java
@@ -2,12 +2,15 @@ package com.github.gymodo.adapters;
 
 import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,7 +22,9 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class UserReservationsAdapter extends RecyclerView.Adapter<UserReservationsAdapter.MyViewHolder> {
 
@@ -27,7 +32,7 @@ public class UserReservationsAdapter extends RecyclerView.Adapter<UserReservatio
     private Context mContext;
     private List<Reservation> mReservations;
 
-    SimpleDateFormat sdf=new SimpleDateFormat("dd/MM/YYYY");
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
 
 
     public UserReservationsAdapter(Context mContext, List<Reservation> mReservations) {
@@ -44,10 +49,10 @@ public class UserReservationsAdapter extends RecyclerView.Adapter<UserReservatio
         return new UserReservationsAdapter.MyViewHolder(view);
     }
 
-    String displayMinutes(int m){
+    String displayMinutes(int m) {
         String min = m + "";
 
-        if (m < 10){
+        if (m < 10) {
             min = "0" + m;
         }
 
@@ -56,6 +61,8 @@ public class UserReservationsAdapter extends RecyclerView.Adapter<UserReservatio
 
     @Override
     public void onBindViewHolder(@NonNull UserReservationsAdapter.MyViewHolder holder, int position) {
+
+        Reservation reservationTmp = mReservations.get(position);
 
         Date date = mReservations.get(position).getDate();
         Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
@@ -67,13 +74,38 @@ public class UserReservationsAdapter extends RecyclerView.Adapter<UserReservatio
         Date endTimeDate = new Date(timeInSecs + (mReservations.get(position).getDuration() * 60 * 1000));
         calendar.setTime(endTimeDate);
 
-        holder.reserv_row_end_time.setText( calendar.get(Calendar.HOUR_OF_DAY) + ":" + displayMinutes(calendar.get(Calendar.MINUTE)));
+        holder.reserv_row_end_time.setText(calendar.get(Calendar.HOUR_OF_DAY) + ":" + displayMinutes(calendar.get(Calendar.MINUTE)));
 
         holder.reserv_row_date.setText(sdf.format(date));
 
+        holder.reserv_row_layout.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+                menu.setHeaderTitle("Choose an options");
+                menu.add(0, v.getId(), 1, "Share");
+
+                MenuItem menuItem = menu.add(0, v.getId(), 2, "Delete");
+                menuItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        /*
+                        String currentUserId =  FirebaseAuth.getInstance().getCurrentUser().getUid();
+                        Iterator<String> userIds = reservationTmp.getUserIds().iterator();
+                        while (userIds.hasNext()){
+                            userIds.next();
+                            if (currentUserId.equalsIgnoreCase(userIds.toString())){
+
+                            }
+                        }*/
+                        return true;
+                    }
+                });
+            }
+        });
+
         ObjectAnimator objectAnimator;
 
-        if(position%2 == 0){
+        if (position % 2 == 0) {
 
             objectAnimator = ObjectAnimator.ofFloat(holder.reserv_row_layout, "x", 1000f, 0f);
 
@@ -90,14 +122,12 @@ public class UserReservationsAdapter extends RecyclerView.Adapter<UserReservatio
         return mReservations.size();
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder {
+    public class MyViewHolder extends RecyclerView.ViewHolder{
 
         TextView reserv_row_start_time;
         TextView reserv_row_end_time;
         TextView reserv_row_date;
-
         ConstraintLayout reserv_row_layout;
-
 
 
         public MyViewHolder(@NonNull View itemView) {
@@ -107,9 +137,10 @@ public class UserReservationsAdapter extends RecyclerView.Adapter<UserReservatio
             reserv_row_end_time = itemView.findViewById(R.id.reserv_row_end_time);
             reserv_row_date = itemView.findViewById(R.id.reserv_row_date);
             reserv_row_layout = itemView.findViewById(R.id.reserv_row_layout);
-
-
-
         }
+
     }
+
+
+
 }
