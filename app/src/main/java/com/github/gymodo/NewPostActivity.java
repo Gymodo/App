8e0ea567196d77995e2445b2e5b8c57fc4440c54
shahsawar <shahsawarbadshah@gmail.com>
2021-05-11 17:37:20 +0200

    progress

--- app/src/main/java/com/github/gymodo/NewPostActivity.java
+++ app/src/main/java/com/github/gymodo/NewPostActivity.java
@@ -1,17 +1,37 @@
 package com.github.gymodo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.github.gymodo.social.Post;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -19,13 +39,25 @@ public class NewPostActivity extends AppCompatActivity {
 
     private EditText newPostContent;
     private Button newPostPublishBtn;
+    private ImageView addNewImage;
+    private ImageView postImage;
+    private Button addExistingImage;
 
     private String description;
     private String author;
     private Date date;
     private boolean canPublish = true;
-
     FirebaseAuth firebaseAuth;
+
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+    public static final int CAMERA_PERM_CODE = 101;
+    String currentPhotoPath;
+    private StorageReference mStorageRef;
+
+    private String filePathTmp;
+    private Uri contentUriTmp;
 
 
     @Override
@@ -35,8 +67,11 @@ public class NewPostActivity extends AppCompatActivity {
 
         newPostContent = findViewById(R.id.newPostContent);
         newPostPublishBtn = findViewById(R.id.newPostPublishBtn);
+        addNewImage = findViewById(R.id.addNewImagePost);
+        postImage = findViewById(R.id.newPostImageView);
 
         firebaseAuth = FirebaseAuth.getInstance();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         //TODO make sure there is content to publish to prevent an empty post
 
@@ -56,5 +91,106 @@ public class NewPostActivity extends AppCompatActivity {
             }
         });
 
+        //Add new image
+        addNewImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                askCameraPermission();
+            }
+        });
+
+    }
+
+    private void askCameraPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+            } else {
+                dispatchTakePictureIntent();
+            }
+        } else {
+            dispatchTakePictureIntent();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Set taken image into imageview
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            File f = new File(currentPhotoPath);
+            Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            Uri contentUri = Uri.fromFile(f);
+            mediaScanIntent.setData(contentUri);
+            this.sendBroadcast(mediaScanIntent);
+
+            filePathTmp = f.getName();
+            contentUriTmp = contentUri;
+
+            postImage.setImageURI(contentUriTmp);
+            //uploadImageToFirebase(f.getName(), contentUri);
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+            Log.d("error-while-creating-image", ex + "");
+
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.example.android.fileprovider",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        }
+
+    }
+
+    public void uploadImageToFirebase(String name, Uri contentUri) {
+
+        File f = new File("images", FirebaseAuth.getInstance().getUid() + "/" + name);
+
+        StorageReference imageRef = mStorageRef.child(f.toString());
+        imageRef.putFile(contentUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                imageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Toast.makeText(NewPostActivity.this, "Upload Image Url", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(NewPostActivity.this, "Image Upload Failled!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
