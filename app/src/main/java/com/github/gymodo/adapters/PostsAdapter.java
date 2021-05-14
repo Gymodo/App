f3e83d8a892440293d223f1b4532d31009dfdf44
shahsawar <shahsawarbadshah@gmail.com>
2021-05-14 18:25:53 +0200

    progress

--- app/src/main/java/com/github/gymodo/adapters/PostsAdapter.java
+++ app/src/main/java/com/github/gymodo/adapters/PostsAdapter.java
@@ -6,10 +6,12 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -17,11 +19,15 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.github.gymodo.CommentsActivity;
 import com.github.gymodo.Constants;
 import com.github.gymodo.R;
+import com.github.gymodo.exercise.Routine;
+import com.github.gymodo.fragments.WorkoutDetailFragment;
 import com.github.gymodo.reservation.Reservation;
 import com.github.gymodo.social.Comment;
 import com.github.gymodo.social.Post;
@@ -84,38 +90,35 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder
 
                         try {
 
-                            if (value.exists()){
+                            if (value.exists()) {
 
                                 Post p = value.toObject(Post.class);
-                                if (p.getDescription() != null){
+                                if (p.getDescription() != null) {
 
                                     holder.postContent.setText(p.getDescription());
                                 }
 
-                                if (p.getCommentIds() != null && p.getCommentIds().size() > 0){
+                                if (p.getCommentIds() != null && p.getCommentIds().size() > 0) {
                                     holder.commentNum.setText(p.getCommentIds().size() + "");
                                 }
 
-                                if (p.getLikedByIds() != null){
+                                if (p.getLikedByIds() != null) {
 
-                                    if (p.getLikedByIds().size() > 0){
+                                    if (p.getLikedByIds().size() > 0) {
                                         holder.likeNum.setText(p.getLikedByIds().size() + "");
                                     } else {
                                         holder.likeNum.setText("");
                                     }
                                 }
 
-                                if (p.getImageUrl() == null){
-                                    holder.image.setVisibility(View.GONE);
-                                }
 
-                                if (p.getLikedByIds().contains(userID)){
+                                if (p.getLikedByIds().contains(userID)) {
                                     holder.postRowLike.setImageResource(R.drawable.ic_like_icon);
                                 }
-                            }else {
+                            } else {
                             }
 
-                        } catch (Exception e){
+                        } catch (Exception e) {
                             Log.d("error", "Exception: " + e);
                         }
                     }
@@ -163,31 +166,55 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder
             holder.image.setVisibility(View.GONE);
         }
 
+        if (post.getRoutineId() != null) {
+            Routine.getByID(post.getRoutineId()).addOnSuccessListener(new OnSuccessListener<Routine>() {
+                @Override
+                public void onSuccess(Routine routine) {
+                    holder.workout.setText(routine.getName());
+                }
+            });
+
+            holder.workout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    NavController navController = Navigation.findNavController(v);
+                    Bundle bundle = new Bundle();
+                    bundle.putString(WorkoutDetailFragment.ARG_ROUTINE_ID, post.getRoutineId());
+                    navController.navigate(R.id.workout_list_to_detail, bundle);
+                }
+            });
+        } else {
+            holder.workout.setVisibility(View.GONE);
+        }
+
+
+
+
         if (post.getLikedByIds().contains(userID)) {
             holder.postRowLike.setImageResource(R.drawable.ic_like_icon);
         }
 
 
         holder.postRowLike.setOnClickListener(v -> {
-           if (post.getLikedByIds().contains(userID)){
+            if (post.getLikedByIds().contains(userID)) {
 
-               post.getLikedByIds().remove(userID);
-               post.update().addOnSuccessListener(new OnSuccessListener<Void>() {
-                   @Override
-                   public void onSuccess(Void aVoid) {
-                       holder.postRowLike.setImageResource(R.drawable.ic_like_icon_outline);
-                   }
-               });
+                post.getLikedByIds().remove(userID);
+                post.update().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        holder.postRowLike.setImageResource(R.drawable.ic_like_icon_outline);
+                    }
+                });
 
-           } else {
-               post.getLikedByIds().add(userID);
-               post.update().addOnSuccessListener(new OnSuccessListener<Void>() {
-                   @Override
-                   public void onSuccess(Void aVoid) {
-                       holder.postRowLike.setImageResource(R.drawable.ic_like_icon);
-                   }
-               });
-           }
+            } else {
+                post.getLikedByIds().add(userID);
+                post.update().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        holder.postRowLike.setImageResource(R.drawable.ic_like_icon);
+                    }
+                });
+            }
             animateLikeIcon(holder.postRowLike);
         });
 
@@ -198,7 +225,6 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder
         });
 
         holder.commentNum.setOnClickListener(v -> {
-
 
             opencomments(mPosts.get(position).getId());
         });
@@ -211,14 +237,14 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder
         Intent intent = new Intent(mContext, CommentsActivity.class);
         intent.putExtra("postID", postId);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         }
 
         mContext.startActivity(intent);
     }
 
-    void animateLikeIcon(ImageView icon){
+    void animateLikeIcon(ImageView icon) {
 
         ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(icon, "scaleX", 0f, 1.5f);
         ObjectAnimator objectAnimator2 = ObjectAnimator.ofFloat(icon, "scaleY", 0f, 1.5f);
@@ -251,6 +277,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder
         TextView postContent;
         TextView likeNum;
         TextView commentNum;
+        TextView workout;
 
         ImageView image;
         ImageView userAvatar;
@@ -268,6 +295,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.MyViewHolder
             userAvatar = itemView.findViewById(R.id.postRowUserAvatar);
             commentIcon = itemView.findViewById(R.id.postRowComment);
             postRowLike = itemView.findViewById(R.id.postRowLike);
+            workout = itemView.findViewById(R.id.postRowRoutine);
 
         }
     }
